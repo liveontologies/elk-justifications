89118744bf1c581d84c39f02229af65fca198e4b
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2016-04-14 10:41:18 +0200

    Simplify Bloom filter used in the custom HashSet
    Now optimizes only set inclusion checking and not element containment.
    For checking set inclusion, Bloom filter with one hash function seems
    most effective.

--- src/main/java/org/semanticweb/elk/justifications/BloomHashSet.java
+++ src/main/java/org/semanticweb/elk/justifications/BloomHashSet.java
@@ -8,8 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A set enhanced with a Bloom filter to quickly check containment of elements.
- * As common with Bloom filters, removal of elements is not supported.
+ * A set enhanced with a Bloom filter to quickly check set inclusion. The Bloom
+ * filter uses just one hash function; containment of elements is not optimized.
+ * As it is common with Bloom filters, removal of elements is not supported.
  * 
  * @see Set#contains(Object)
  * @see Set#containsAll(Collection)
@@ -31,8 +32,9 @@ class BloomHashSet<E> extends HashSet<E> {
 	private static int STATS_CONTAINS_ALL_COUNT_ = 0,
 			STATS_CONTAINS_ALL_FILTERED_ = 0;
 
-	private static final short SHIFT_ = 6;
+	private static final short SHIFT_ = 6; // 2^6 = 64
 
+	// = 11..1 SHIFT_ times
 	private static final int MASK_ = (1 << SHIFT_) - 1;
 
 	private long filter_ = 0L;
@@ -54,29 +56,9 @@ class BloomHashSet<E> extends HashSet<E> {
 	public boolean add(E e) {
 		boolean success = super.add(e);
 		if (success) {
-			int hash = e.hashCode();
-			filter_ |= 1 << (hash & MASK_);
-			hash >>= SHIFT_;
-			filter_ |= 1 << (hash & MASK_);
-			hash >>= SHIFT_;
-			filter_ |= 1 << (hash & MASK_);
+			filter_ |= 1 << (e.hashCode() & MASK_);
 		}
 		return success;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		int hash = o.hashCode();
-		long mask = 1 << (hash & MASK_);
-		hash >>= SHIFT_;
-		mask |= 1 << (hash & MASK_);
-		hash >>= SHIFT_;
-		mask |= 1 << (hash & MASK_);
-		if ((filter_ & mask) != mask) {
-			return false;
-		}
-		// else
-		return super.contains(o);
 	}
 
 	@Override
@@ -105,11 +87,7 @@ class BloomHashSet<E> extends HashSet<E> {
 				return false;
 			}
 		}
-		for (Object e : c)
-			// no need to apply filter for contains test
-			if (!super.contains(e))
-				return false;
-		return true;
+		return super.containsAll(c);
 	}
 
 	public static void printStatistics() {
