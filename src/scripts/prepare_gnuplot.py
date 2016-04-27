d307a8dc85f0a1c814dd0b6832a7ad463b847e58
Peter Skocovsky <peter.skocovsky@uni-ulm.de>
2016-04-27 20:59:04 +0200

    Improve the plotting script

--- src/scripts/prepare_gnuplot.py
+++ src/scripts/prepare_gnuplot.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import sys
+import sys, os
 import argparse
 import csv
 from string import Template
 
 parser = argparse.ArgumentParser(description='Prepare gnuplot script from the supplied data files.')
-parser.add_argument('files', nargs='+', help='The data files.')
+parser.add_argument('-t', '--title', default='', help='The plot title.')
+parser.add_argument('data', nargs='+', help='Sequence of the data files, ' +
+				'each of which may be followed by names of columns that should be plotted.')
 
 MIN_Y_RANGE = 0.000001
+DEFAULT_COLUMN = 'time'
 
 GNUPLOT_SCRIPT_TEMPLATE = Template("""
 
@@ -17,7 +20,7 @@ reset
 set terminal lua tikz latex
 set output "plot.tex"
 
-#set title "tau_m"
+${set_title}
 set style data lines
 set key left top
 set logscale y
@@ -40,48 +43,82 @@ pause -1
 if __name__ == "__main__":
 	
 	args = parser.parse_args()
+	if not os.path.exists(args.data[0]):
+		parser.error('the first data is not a file')
+		
+	# TODO: debug
+	print args
+	
+	input_data = {}
+	current_file = None
+	current_columns = []
+	for arg in args.data:
+		if os.path.exists(arg):
+			# save the collected data and start collecting new ones
+			if current_file:
+				if current_columns:
+					input_data[current_file] = current_columns
+				else:
+					input_data[current_file] = [DEFAULT_COLUMN]
+			current_file = arg
+			current_columns = []
+		else:
+			current_columns.append(arg)
+	if current_columns:
+		input_data[current_file] = current_columns
+	else:
+		input_data[current_file] = [DEFAULT_COLUMN]
+	
+	print input_data
 	
 	plot_cmd = ""
 	data_string = ""
 	min_data = sys.float_info.max
 	max_data = sys.float_info.min
-	for data_file in args.files:
-		
-		plot_cmd += """'-' title "%s", """ % data_file
-		
-		with open(data_file) as fd:
-			reader = csv.reader(fd, delimiter=',', quotechar='"')
-			header = reader.next()
-#			print header
-#			time_index = header.index('time')
-			time_index = 0
-			for h in header:
-				if h.find('time') >= 0:
-					break
-				time_index += 1
-			data = []
-			for line in reader:
-				data.append(float(line[time_index])/1000)
-			data.sort()
+	for data_file in input_data.keys():
+		for column in input_data[data_file]:
 			
-			if data[0] < min_data:
-				min_data = data[0]
-			if data[len(data) - 1] > max_data:
-				max_data = data[len(data) - 1]
+			if column == DEFAULT_COLUMN:
+				plot_cmd += """'-' title "%s", """ % data_file
+			else:
+				plot_cmd += """'-' title "%s %s", """ % (data_file, column)
 			
-			step = 100.0/len(data)
+			with open(data_file) as fd:
+				reader = csv.reader(fd, delimiter=',', quotechar='"')
+				header = reader.next()
+	#			print header
+				time_index = header.index(column)
+# 				time_index = 0
+# 				for h in header:
+# 					if h.find(column) >= 0:
+# 						break
+# 					time_index += 1
+				data = []
+				for line in reader:
+					data.append(float(line[time_index])/1000)
+				data.sort()
+				
+				if data[0] < min_data:
+					min_data = data[0]
+				if data[len(data) - 1] > max_data:
+					max_data = data[len(data) - 1]
+				
+				step = 100.0/len(data)
+				
+				x = step
+				for d in data:
+					data_string += "%f\t%f\n" % (x, d)
+					x += step
+				data_string += "e\n"
 			
-			x = step
-			for d in data:
-				data_string += "%f\t%f\n" % (x, d)
-				x += step
-			data_string += "e\n"
-		
-		pass
+			pass
 	
 	min_data = max(min_data, MIN_Y_RANGE)
 	
-	print GNUPLOT_SCRIPT_TEMPLATE.substitute(plot_cmd=plot_cmd, data=data_string, lower_y_range=min_data, upper_y_range=max_data)
+	set_title = ''
+	if args.title:
+		set_title = 'set title "%s"' % args.title
+	print GNUPLOT_SCRIPT_TEMPLATE.substitute(set_title=set_title, plot_cmd=plot_cmd, data=data_string, lower_y_range=min_data, upper_y_range=max_data)
 	
 	pass
 
