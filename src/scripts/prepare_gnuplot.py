acd64aa251690a4823885864ea8b1ef03706ec00
Peter Skocovsky <peter.skocovsky@uni-ulm.de>
2016-04-28 17:27:50 +0200

    Plot only top few percent

--- src/scripts/prepare_gnuplot.py
+++ src/scripts/prepare_gnuplot.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description='Prepare gnuplot script from the su
 parser.add_argument('-t', '--title', default='', help='The plot title.')
 parser.add_argument('data', nargs='+', help='Sequence of the data files, ' +
 				'each of which may be followed by names of columns that should be plotted.')
+parser.add_argument('-c', '--cut_top', type=float, default=100.0, help='How many top % of the data should be plotted.')
 
 MIN_Y_RANGE = 0.000001
 DEFAULT_COLUMN = 'time'
@@ -45,9 +46,9 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	if not os.path.exists(args.data[0]):
 		parser.error('the first data is not a file')
-		
-	# TODO: debug
-	print args
+	
+	# TODO debug !!!
+#	print args
 	
 	input_data = {}
 	current_file = None
@@ -68,8 +69,6 @@ if __name__ == "__main__":
 		input_data[current_file] = current_columns
 	else:
 		input_data[current_file] = [DEFAULT_COLUMN]
-	
-	print input_data
 	
 	plot_cmd = ""
 	data_string = ""
@@ -93,18 +92,22 @@ if __name__ == "__main__":
 # 					if h.find(column) >= 0:
 # 						break
 # 					time_index += 1
+				# collect the data
 				data = []
 				for line in reader:
 					data.append(float(line[time_index])/1000)
 				data.sort()
+				
+				# cut top
+				data = data[int(len(data) * (1.0 - (args.cut_top/100))):]
 				
 				if data[0] < min_data:
 					min_data = data[0]
 				if data[len(data) - 1] > max_data:
 					max_data = data[len(data) - 1]
 				
+				# TODO .: set x_tics according to the cut !!!
 				step = 100.0/len(data)
-				
 				x = step
 				for d in data:
 					data_string += "%f\t%f\n" % (x, d)
