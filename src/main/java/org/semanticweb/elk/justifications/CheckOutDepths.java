06c21e6a11f4947b222909dbcc93afd790505e92
Peter Skocovsky <peter.skocovsky@uni-ulm.de>
2016-05-17 11:01:03 +0200

    Search for sub-inference-sets whose reducrion simplifies the search for justifications

--- src/main/java/org/semanticweb/elk/justifications/CheckOutDepths.java
+++ src/main/java/org/semanticweb/elk/justifications/CheckOutDepths.java
@@ -1,11 +1,15 @@
 package org.semanticweb.elk.justifications;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.semanticweb.elk.exceptions.ElkException;
@@ -18,8 +22,8 @@ import org.semanticweb.elk.owl.iris.ElkFullIri;
 import org.semanticweb.elk.owl.parsing.javacc.Owl2FunctionalStyleParserFactory;
 import org.semanticweb.elk.proofs.Inference;
 import org.semanticweb.elk.proofs.InferenceSet;
+import org.semanticweb.elk.proofs.adapters.DepthLimitInferenceSetAdapter;
 import org.semanticweb.elk.proofs.adapters.DepthLimitInferenceSetAdapter.Wrap;
-import org.semanticweb.elk.proofs.browser.ProofBrowser;
 import org.semanticweb.elk.proofs.adapters.InferenceSets;
 import org.semanticweb.elk.proofs.adapters.TracingInferenceSetInferenceSetAdapter;
 import org.semanticweb.elk.reasoner.ElkInconsistentOntologyException;
@@ -41,18 +45,24 @@ public class CheckOutDepths {
 	
 	public static void main(final String[] args) {
 		
-		if (args.length < 3) {
+		if (args.length < 5) {
 			LOG.error("Insufficient arguments!");
 			System.exit(1);
 		}
 		
-		final String ontologyFileName = args[0];
-		final String subFullIri = args[1];
-		final String supFullIri = args[2];
+		final File recordFile = new File(args[0]);
+		if (recordFile.exists()) {
+			Utils.recursiveDelete(recordFile);
+		}
+		final int depthLimit = Integer.parseInt(args[1]);
+		final String ontologyFileName = args[2];
+		final String subFullIri = args[3];
+		final String supFullIri = args[4];
 		
 		final ElkObjectBaseFactory factory = new ElkObjectBaseFactory();
 		
 		InputStream ontologyIS = null;
+		PrintWriter record = null;
 		
 		try {
 			
@@ -78,8 +88,10 @@ public class CheckOutDepths {
 			final InferenceSet<Conclusion, ElkAxiom> inferenceSet =
 					new TracingInferenceSetInferenceSetAdapter(
 							reasoner.explainConclusion(expression));
+			
+			record = new PrintWriter(recordFile);
 
-			checkOutDepths(inferenceSet, expression);
+			checkOutDepths(inferenceSet, expression, depthLimit, record);
 			
 		} catch (final FileNotFoundException e) {
 			LOG.error("File Not Found!", e);
@@ -96,47 +108,194 @@ public class CheckOutDepths {
 					ontologyIS.close();
 				} catch (final IOException e) {}
 			}
+			if (record != null) {
+				record.close();
+			}
 		}
 		
 	}
 	
-	public static <C, A> void checkOutDepths(
-			final InferenceSet<C, A> original, final C conclusion) {
+	public static <C, A> void checkOutDepths(final InferenceSet<C, A> original,
+			final C goalConclusion, final int maxDepth,
+			final PrintWriter record) {
 		
-		ProofBrowser.showProofBrowser(original, conclusion);
+		record.println("conclusion,nLeaves,nConclusionsInDepthLimit,nInferencesInDepthLimit,avgInfsOfConcl,leafProductSum,minLeafProductSum,productSum,minProductSum,unusedLeaves,symbolicProductSum,symbolicMinProductSum,nonRedundantProductSum,nonRedundantMinProductSum");
 		
-		for (int maxDepth = 1; maxDepth <= 10; maxDepth++) {// TODO: actual proof depth !!!
-			LOG.info("Checking depth: {}", maxDepth);
+		// collect conclusions
+		final Set<C> conclusions = new HashSet<C>();
+		Utils.traverseProofs(goalConclusion, original,
+				Functions.<Inference<C, A>>identity(),
+				new Function<C, Void>() {
+					@Override
+					public Void apply(final C conclusion) {
+						conclusions.add(conclusion);
+						return null;
+					}
+				},
+				Functions.<A>identity());
+		
+		final JustificationComputation<C, A> computation =
+				BottomUpJustificationComputation.<C, A>getFactory()
+				.create(original, new DummyMonitor());
+		
+		LOG.info("computing justifications");
+		computation.computeJustifications(goalConclusion);
+		
+		for (final C conclusion : conclusions) {
+			LOG.info("For conclusion: {}", conclusion);
+			record.print('"');
+			record.print(conclusion);
+			record.print('"');
 			
 			LOG.info("limiting depth");
 			final InferenceSet<C, Wrap<C, A>> limited =
 					InferenceSets.limitDepth(original, maxDepth, conclusion);
 			
-			ProofBrowser.showProofBrowser(limited, conclusion);
-//			ProofBrowser.showProofBrowser(limited, conclusion, "limited to " + maxDepth);
-			
 			LOG.info("collecting leaves");
+			final Set<Inference<C, Wrap<C, A>>> infs = new HashSet<>();
 			final Set<Wrap<C, A>> leaves = new HashSet<Wrap<C, A>>();
+			final Set<C> conclsInDepthLimit = new HashSet<C>();
+			final List<Integer> sumInfsPerConcl = Arrays.asList(0);
 			Utils.traverseProofs(conclusion, limited,
 					new Function<Inference<C, Wrap<C, A>>, Void>() {
 						@Override
 						public Void apply(final Inference<C, Wrap<C, A>> inf) {
-							leaves.addAll(inf.getJustification());
+							infs.add(inf);
 							return null;
 						}
 					},
-					Functions.<C>identity(),
-					Functions.<Wrap<C, A>>identity());
+					new Function<C, Void>() {
+						@Override
+						public Void apply(final C concl) {
+							conclsInDepthLimit.add(concl);
+							for (@SuppressWarnings("unused")
+									final Inference<C, Wrap<C, A>> inf
+									: limited.getInferences(concl)) {
+								sumInfsPerConcl.set(0, sumInfsPerConcl.get(0) + 1);
+							}
+							return null;
+						}
+					},
+					new Function<Wrap<C, A>, Void>() {
+						@Override
+						public Void apply(final Wrap<C, A> axiom) {
+							leaves.add(axiom);
+							return null;
+						}
+					});
+			LOG.info("{} = number of leaves", leaves.size());
+			LOG.info("{} = number of conclusions within the depth limit", conclsInDepthLimit.size());
+			LOG.info("{} = number of inferences within the depth limit", infs.size());
+			LOG.info("{} = average number of inferences of a conclusion", ((double) sumInfsPerConcl.get(0)) / conclsInDepthLimit.size());
+			record.print(',');
+			record.print(leaves.size());
+			record.print(',');
+			record.print(conclsInDepthLimit.size());
+			record.print(',');
+			record.print(infs.size());
+			record.print(',');
+			record.print(((double) sumInfsPerConcl.get(0)) / conclsInDepthLimit.size());
 			
-			final JustificationComputation<C, Wrap<C, A>> computation =
+			if (infs.size() > 10 * maxDepth) {
+				LOG.info("Too many inferences, skipping");
+				record.println();
+				continue;
+			}
+			
+			// product of justifications of leaves
+			
+			// product of premise-minimal justifications of leaves
+			
+			// sum of products of justifications of all conclusions within the depth limit
+			
+			// sum of products of premise-minimal justifications of all conclusions within the depth limit
+			long leafProductSum = 0;
+			long minLeafProductSum = 0;
+//			long sum = 0;
+			long productSum = 0;
+			long minProductSum = 0;
+			for (final Inference<C, Wrap<C, A>> inf : infs) {
+				
+				final Collection<? extends Set<A>> conclJs =
+						computation.computeJustifications(inf.getConclusion());
+				
+				long leafProduct = 1;
+				long minLeafProduct = 1;
+				long product = 1;
+				long minProduct = 1;
+				for (final C premise : inf.getPremises()) {
+					
+					final Collection<? extends Set<A>> js =
+							computation.computeJustifications(premise);
+					
+					product *= js.size();
+					
+					long count = 0;
+					for (final Set<A> just : js) {
+						if (BottomUpJustificationComputation.isMinimal(
+								new BloomSet<>(inf.getConclusion(), just, inf.getJustification()),
+								conclJs)) {
+							count++;
+						}
+					}
+					minProduct *= count;
+//					sum += count;
+					
+				}
+				for (final Wrap<C, A> leaf : inf.getJustification()) {
+					if (leaf instanceof DepthLimitInferenceSetAdapter.ConclusionWrap) {
+						final C premise = ((DepthLimitInferenceSetAdapter.ConclusionWrap<C, A>) leaf).conclusion;
+						
+						final Collection<? extends Set<A>> js =
+								computation.computeJustifications(premise);
+						
+						leafProduct *= js.size();
+						product *= js.size();
+						
+						long count = 0;
+						for (final Set<A> just : js) {
+							if (BottomUpJustificationComputation.isMinimal(
+									new BloomSet<>(inf.getConclusion(), just, inf.getJustification()),
+									conclJs)) {
+								count++;
+							}
+						}
+						minLeafProduct *= count;
+						minProduct *= count;
+//						sum += count;
+						
+					}
+					
+				}
+				
+				leafProductSum += leafProduct;
+				minLeafProductSum += minLeafProduct;
+				productSum += product;
+				minProductSum += minProduct;
+			}
+			
+			LOG.info("{} = sum of products of justifications of leaves", leafProductSum);
+			LOG.info("{} = sum of products of premise-minimal justifications of leaves", minLeafProductSum);
+			LOG.info("{} = sum of products of justifications of all conclusions within the depth limit", productSum);
+			LOG.info("{} = sum of products of premise-minimal justifications of all conclusions within the depth limit", minProductSum);
+			record.print(',');
+			record.print(leafProductSum);
+			record.print(',');
+			record.print(minLeafProductSum);
+			record.print(',');
+			record.print(productSum);
+			record.print(',');
+			record.print(minProductSum);
+			
+			final JustificationComputation<C, Wrap<C, A>> limitComputation =
 					BottomUpJustificationComputation.<C, Wrap<C, A>>getFactory()
 					.create(limited, new DummyMonitor());
 			
-			LOG.info("computing justifications");
-			final Collection<? extends Set<Wrap<C, A>>> justifications =
-					computation.computeJustifications(conclusion);
+			LOG.info("computing justifications in the limited inference set");
+			final Collection<? extends Set<Wrap<C, A>>> limitJusts =
+					limitComputation.computeJustifications(conclusion);
 			
-			for (final Set<Wrap<C, A>> justification : justifications) {
+			for (final Set<Wrap<C, A>> justification : limitJusts) {
 				leaves.removeAll(justification);
 			}
 			
@@ -145,11 +304,104 @@ public class CheckOutDepths {
 			} else {
 				LOG.info("{} UNUSED LEAVES: {}", leaves.size(), leaves);
 			}
+			record.print(',');
+			record.print(leaves.size());
 			
-			System.out.println("Press any key to continue.");
-			try {
-				System.in.read();
-			} catch (IOException e) {}
+			// sum of products of symbolic justifications of all conclusions
+			
+			// sum of products of premise-minimal symbolic justifications of all conclusions
+			long symbolicProductSum = 0;
+			long symbolicMinProductSum = 0;
+			for (final Inference<C, Wrap<C, A>> inf : infs) {
+				
+				final Collection<? extends Set<Wrap<C, A>>> conclJs =
+						limitComputation.computeJustifications(inf.getConclusion());
+				
+				long product = 1;
+				long minProduct = 1;
+				for (final C premise : inf.getPremises()) {
+					
+					final Collection<? extends Set<Wrap<C, A>>> js =
+							limitComputation.computeJustifications(premise);
+					
+					product *= js.size();
+					
+					long count = 0;
+					for (final Set<Wrap<C, A>> just : js) {
+						if (BottomUpJustificationComputation.isMinimal(
+								new BloomSet<>(inf.getConclusion(), just, inf.getJustification()),
+								conclJs)) {
+							count++;
+						}
+					}
+					minProduct *= count;
+					
+				}
+				
+				symbolicProductSum += product;
+				symbolicMinProductSum += minProduct;
+			}
+			
+			LOG.info("{} = sum of products of symbolic justifications of all conclusions", symbolicProductSum);
+			LOG.info("{} = sum of products of premise-minimal symbolic justifications of all conclusions", symbolicMinProductSum);
+			record.print(',');
+			record.print(symbolicProductSum);
+			record.print(',');
+			record.print(symbolicMinProductSum);
+			
+			// product of justifications of non-redundant leaves
+			
+			// product of premise-minimal justifications of non-redundant leaves
+			final Collection<? extends Set<A>> conclJs =
+					computation.computeJustifications(conclusion);
+			long nonRedundantProductSum = 0;
+			long nonRedundantMinProductSum = 0;
+			for (final Set<Wrap<C, A>> justification : limitJusts) {
+				
+				final Set<A> axiomsInLeaves = new HashSet<A>();
+				for (final Wrap<C, A> leaf : justification) {
+					if (leaf instanceof DepthLimitInferenceSetAdapter.AxiomWrap) {
+						axiomsInLeaves.add(((DepthLimitInferenceSetAdapter.AxiomWrap<C, A>) leaf).axiom);
+					}
+				}
+				
+				long product = 1;
+				long minProduct = 1;
+				for (final Wrap<C, A> leaf : justification) {
+					if (leaf instanceof DepthLimitInferenceSetAdapter.ConclusionWrap) {
+						final C concl = ((DepthLimitInferenceSetAdapter.ConclusionWrap<C, A>) leaf).conclusion;
+						
+						final Collection<? extends Set<A>> js =
+								computation.computeJustifications(concl);
+						
+						product *= js.size();
+						
+						long count = 0;
+						for (final Set<A> just : js) {
+							if (BottomUpJustificationComputation.isMinimal(
+									new BloomSet<>(concl, just, axiomsInLeaves),
+									conclJs)) {
+								count++;
+							}
+						}
+						minProduct *= count;
+						
+					}
+				}
+				
+				nonRedundantProductSum += product;
+				nonRedundantMinProductSum += minProduct;
+			}
+			
+			LOG.info("{} = sum of products of justifications of non-redundant leaves", nonRedundantProductSum);
+			LOG.info("{} = sum of products of premise-minimal justifications of non-redundant leaves", nonRedundantMinProductSum);
+			record.print(',');
+			record.print(nonRedundantProductSum);
+			record.print(',');
+			record.print(nonRedundantMinProductSum);
+			
+			record.flush();
+			record.println();
 		}
 		
 	}
--- src/main/java/org/semanticweb/elk/proofs/browser/ProofBrowser.java
+++ src/main/java/org/semanticweb/elk/proofs/browser/ProofBrowser.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.JFrame;
@@ -18,6 +19,7 @@ import org.semanticweb.elk.exceptions.ElkException;
 import org.semanticweb.elk.justifications.BottomUpJustificationComputation;
 import org.semanticweb.elk.justifications.DummyMonitor;
 import org.semanticweb.elk.justifications.JustificationComputation;
+import org.semanticweb.elk.justifications.Utils;
 import org.semanticweb.elk.loading.AxiomLoader;
 import org.semanticweb.elk.loading.Owl2StreamLoader;
 import org.semanticweb.elk.owl.implementation.ElkObjectBaseFactory;
@@ -27,7 +29,9 @@ import org.semanticweb.elk.owl.iris.ElkFullIri;
 import org.semanticweb.elk.owl.parsing.javacc.Owl2FunctionalStyleParserFactory;
 import org.semanticweb.elk.proofs.Inference;
 import org.semanticweb.elk.proofs.InferenceSet;
+import org.semanticweb.elk.proofs.adapters.InferenceSets;
 import org.semanticweb.elk.proofs.adapters.TracingInferenceSetInferenceSetAdapter;
+import org.semanticweb.elk.proofs.adapters.DepthLimitInferenceSetAdapter.Wrap;
 import org.semanticweb.elk.reasoner.ElkInconsistentOntologyException;
 import org.semanticweb.elk.reasoner.Reasoner;
 import org.semanticweb.elk.reasoner.ReasonerFactory;
@@ -36,6 +40,8 @@ import org.semanticweb.elk.reasoner.stages.RestartingStageExecutor;
 import org.semanticweb.elk.reasoner.tracing.Conclusion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
 
 public class ProofBrowser {
 	
@@ -72,102 +78,108 @@ public class ProofBrowser {
 			reasoner.getTaxonomy();
 			LOG.info("... took {}s",
 					(System.currentTimeMillis() - start)/1000.0);
+//			
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//					factory.getClass(new ElkFullIri(subFullIri)),
+//					factory.getClass(new ElkFullIri(supFullIri)));
 			
+			// bad
+			
+			// [:GO_2000370] ⊑ <∃:RO_0002211>.:GO_0008150
+			// Huge number of alternative inferences
+			// 28 PgDown
+			// Contains many cycles
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//					factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//					factory.getObjectSomeValuesFrom(
+//							factory.getObjectProperty(new ElkFullIri("http://purl.obolibrary.org/obo/RO_0002211")),
+//							factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0008150"))));
+			
+			// [:GO_2000370] ⊑ <∃:RO_0002211>.:GO_0009987
+			// 15 PgDown
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getObjectSomeValuesFrom(
+//					factory.getObjectProperty(new ElkFullIri("http://purl.obolibrary.org/obo/RO_0002211")),
+//					factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0009987"))));
+			
+			// good
+			
+			// [:GO_2000370] ⊑ -:GO_2000369
+			// There is a tautology that has a huge alternative
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000369")));
+			
+			// [:GO_2000370] ⊑ -∃:RO_0002213.:GO_0006810
+			// There was cut just through premises of many alternative infs
+			// 8 PgDown
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getObjectSomeValuesFrom(
+//					factory.getObjectProperty(new ElkFullIri("http://purl.obolibrary.org/obo/RO_0002213")),
+//					factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0006810"))));
+			
+			// [:GO_2000370] ⊑ +:GO_0030100
+			// 24 PgDown
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0030100")));
+			
+			// depth 5
+			
+			// [:GO_2000370] ⊑ +:GO_0050789
+			// Huge alternatives
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0050789")));
+			
+			// depth 9
+			
+			// bad
+			
+			// [:GO_2000370] ⊑ +∃:RO_0002211.:GO_0051179
+			// Huge alternatives
+//			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
+//			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+//			factory.getObjectSomeValuesFrom(
+//					factory.getObjectProperty(new ElkFullIri("http://purl.obolibrary.org/obo/RO_0002211")),
+//					factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0051179"))));
+			
+			// good
+			
+			// [:GO_2000370] ⊑ +∃:RO_0002211.:GO_0072583
+			// A bit less alternatives :-P
 			final ElkSubClassOfAxiom conclusion = factory.getSubClassOfAxiom(
-					factory.getClass(new ElkFullIri(subFullIri)),
-					factory.getClass(new ElkFullIri(supFullIri)));
+			factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_2000370")),
+			factory.getObjectSomeValuesFrom(
+					factory.getObjectProperty(new ElkFullIri("http://purl.obolibrary.org/obo/RO_0002211")),
+					factory.getClass(new ElkFullIri("http://purl.obolibrary.org/obo/GO_0072583"))));
 			
 			final ClassConclusion expression =
 					reasoner.getConclusion(conclusion);
 			final InferenceSet<Conclusion, ElkAxiom> inferenceSet =
 					new TracingInferenceSetInferenceSetAdapter(
 							reasoner.explainConclusion(expression));
+			
+			final InferenceSet<Conclusion, Wrap<Conclusion, ElkAxiom>> limited =
+					InferenceSets.limitDepth(inferenceSet, 9, expression);
 
-			final JustificationComputation<Conclusion, ElkAxiom> computation =
+//			final JustificationComputation<Conclusion, ElkAxiom> computation =
+//					BottomUpJustificationComputation
+//					.<Conclusion, ElkAxiom> getFactory()
+//					.create(inferenceSet, DummyMonitor.INSTANCE);
+			final JustificationComputation<Conclusion, Wrap<Conclusion, ElkAxiom>> computation =
 					BottomUpJustificationComputation
-					.<Conclusion, ElkAxiom> getFactory()
-					.create(inferenceSet, DummyMonitor.INSTANCE);
+					.<Conclusion, Wrap<Conclusion, ElkAxiom>> getFactory()
+					.create(limited, DummyMonitor.INSTANCE);
 			
 			for (int size = startingSizeLimit; size <= Integer.MAX_VALUE; size++) {
 				
-				final int sizeLimit = size;
-				
-				final Collection<? extends Set<ElkAxiom>> justs =
-						computation.computeJustifications(expression, sizeLimit);
-				
-				final TreeNodeLabelProvider decorator = new TreeNodeLabelProvider() {
-					@Override
-					public String getLabel(final Object obj,
-							final TreePath path) {
-						
-						if (obj instanceof Conclusion) {
-							final Conclusion c = (Conclusion) obj;
-							final Collection<? extends Set<ElkAxiom>> js =
-									computation.computeJustifications(c, sizeLimit);
-							return "[" + js.size() + "] ";
-						} else if (obj instanceof Inference) {
-							final Inference<?, ?> inf = (Inference<?, ?>) obj;
-							int product = 1;
-							for (final Object premise : inf.getPremises()) {
-								final Collection<? extends Set<ElkAxiom>> js =
-										computation.computeJustifications((Conclusion) premise, sizeLimit);
-								product *= js.size();
-							}
-							return "<" + product + "> ";
-						}
-						
-						return "";
-					}
-				};
-				
-				final TreeNodeLabelProvider toolTipProvider = new TreeNodeLabelProvider() {
-					@Override
-					public String getLabel(final Object obj,
-							final TreePath path) {
-						
-						if (path == null || path.getPathCount() < 2
-								|| !(obj instanceof Conclusion)) {
-							return null;
-						}
-						final Conclusion premise = (Conclusion) obj;
-						final Object o = path.getPathComponent(path.getPathCount() - 2);
-						if (!(o instanceof Inference)) {
-							return null;
-						}
-						final Inference<?, ?> inf = (Inference<?, ?>) o;
-						final Object c = inf.getConclusion();
-						if (!(c instanceof Conclusion)) {
-							return null;
-						}
-						final Conclusion concl = (Conclusion) c;
-						
-						final Collection<? extends Set<ElkAxiom>> premiseJs =
-								computation.computeJustifications(premise, sizeLimit);
-						final Collection<? extends Set<ElkAxiom>> conclJs =
-								computation.computeJustifications(concl, sizeLimit);
-						
-						int countInf = 0;
-						for (final Set<ElkAxiom> just : premiseJs) {
-							if (BottomUpJustificationComputation.isMinimal(just, conclJs)) {
-								countInf++;
-							}
-						}
-						
-						int countGoal = 0;
-						for (final Set<ElkAxiom> just : premiseJs) {
-							if (BottomUpJustificationComputation.isMinimal(just, justs)) {
-								countGoal++;
-							}
-						}
-						
-						return "<html>minimal in inf conclusion: " + countInf +
-								"<br/>minimal in goal: " + countGoal +
-								"</html>";
-					}
-				};
-				
-				showProofBrowser(inferenceSet, expression, "size " + sizeLimit,
-						decorator, toolTipProvider);
+//				decorateAndShowProofBrowser(inferenceSet, expression,
+//						computation, size);
+				decorateAndShowProofBrowser(limited, expression,
+						computation, size);
 				
 				try {
 					System.out.print("Press ENTER to continue: ");
@@ -201,6 +213,151 @@ public class ProofBrowser {
 				} catch (final IOException e) {}
 			}
 		}
+		
+	}
+	
+	private static <C, A> void decorateAndShowProofBrowser(
+			final InferenceSet<C, A> inferenceSet, final C conclusion,
+			final JustificationComputation<C, A> computation,
+			final int sizeLimit) {
+		
+		// I didn't find other way to distinguish between lemmas and axioms.
+		final Set<Inference<C, A>> infs = new HashSet<>();
+		final Set<A> axioms = new HashSet<>();
+		final Set<C> concls = new HashSet<>();
+		Utils.traverseProofs(conclusion, inferenceSet,
+				new Function<Inference<C, A>, Void>() {
+					@Override
+					public Void apply(final Inference<C, A> inf) {
+						infs.add(inf);
+						return null;
+					}
+				},
+				new Function<C, Void>() {
+					@Override
+					public Void apply(final C concl) {
+						concls.add(concl);
+						return null;
+					}
+				},
+				new Function<A, Void>() {
+					@Override
+					public Void apply(final A axiom) {
+						axioms.add(axiom);
+						return null;
+					}
+				});
+		
+		final Collection<? extends Set<A>> justs =
+				computation.computeJustifications(conclusion, sizeLimit);
+		
+		final TreeNodeLabelProvider decorator = new TreeNodeLabelProvider() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public String getLabel(final Object obj,
+					final TreePath path) {
+				
+				try {
+					if (obj instanceof Inference) {
+						final Inference<?, ?> inf = (Inference<?, ?>) obj;
+						int product = 1;
+						for (final Object premise : inf.getPremises()) {
+							final Collection<? extends Set<A>> js =
+									computation.computeJustifications((C) premise, sizeLimit);
+							product *= js.size();
+						}
+						return "<" + product + "> ";
+					} else {
+						final C c = (C) obj;
+						if (concls.contains(c)) {
+							final Collection<? extends Set<A>> js =
+									computation.computeJustifications(c, sizeLimit);
+							return "[" + js.size() + "] ";
+						}
+					}
+				} catch (final ClassCastException e) {
+					// obj was not of type C
+				}
+				
+				return "";
+			}
+		};
+		
+		final TreeNodeLabelProvider toolTipProvider = new TreeNodeLabelProvider() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public String getLabel(final Object obj,
+					final TreePath path) {
+				
+				if (path == null || path.getPathCount() < 2) {
+					return null;
+				}
+				boolean isConclusion = false;
+				try {
+					final C premise = (C) obj;
+					if (concls.contains(premise)) {
+						isConclusion = true;
+						final Object o = path.getPathComponent(path.getPathCount() - 2);
+						if (!(o instanceof Inference)) {
+							return null;
+						}
+						final Inference<?, ?> inf = (Inference<?, ?>) o;
+						final Object c = inf.getConclusion();
+						if (!(c instanceof Conclusion)) {
+							return null;
+						}
+						final C concl = (C) c;
+						
+						final Collection<? extends Set<A>> premiseJs =
+								computation.computeJustifications(premise, sizeLimit);
+						final Collection<? extends Set<A>> conclJs =
+								computation.computeJustifications(concl, sizeLimit);
+						
+						int countInf = 0;
+						for (final Set<A> just : premiseJs) {
+							if (BottomUpJustificationComputation.isMinimal(just, conclJs)) {
+								countInf++;
+							}
+						}
+						
+						int countGoal = 0;
+						for (final Set<A> just : premiseJs) {
+							if (BottomUpJustificationComputation.isMinimal(just, justs)) {
+								countGoal++;
+							}
+						}
+						
+						return "<html>minimal in inf conclusion: " + countInf +
+								"<br/>minimal in goal: " + countGoal +
+								"</html>";
+					}
+				} catch (final ClassCastException e) {
+					// obj was not of type C
+				}
+				
+				if (!isConclusion) {
+					try {
+						final A axiom = (A) obj;
+						if (axioms.contains(axiom)) {
+							for (final Set<A> just : justs) {
+								if (just.contains(axiom)) {
+									return "Used in some justification";
+								}
+							}
+							return "Not used in any justification";
+						}
+					} catch (final ClassCastException e) {
+						// obj was not of type A
+					}
+				}
+				
+				// TODO .: product of premise-minimal justs for infs
+				return null;
+			}
+		};
+		
+		showProofBrowser(inferenceSet, conclusion, "size " + sizeLimit,
+				decorator, toolTipProvider);
 		
 	}
 	
