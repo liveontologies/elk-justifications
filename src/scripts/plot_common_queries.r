5389d741bedaa9cec085663a9e933338d2271a8a
Peter Skocovsky <peter.skocovsky@uni-ulm.de>
2017-04-10 16:43:17 +0200

    Improve plotting scripts

--- src/scripts/plot_common_queries.r
+++ src/scripts/plot_common_queries.r
@@ -24,21 +24,15 @@ xvalues = list()
 yvalues = list()
 colors = list()
 
-title <- args[1]
-argIndex <- 2
+argIndex <- 1
+title <- args[argIndex]
+
+argIndex <- argIndex + 1
 legend <- args[argIndex]
 argIndex <- argIndex + 1
 X <- read.csv(args[argIndex])
 
 dataIndex <- 1
-step <- 100 / length(X[[column]])
-xvalues[[dataIndex]] <- seq(step, 100, step)
-timeOrder <- order(X[[column]])
-yvalues[[dataIndex]] <- X[[column]][timeOrder] / 1000
-colorIndex <- 2
-colors[[dataIndex]] <- colorIndex
-legendStyle <- colorIndex
-
 names(X) <- paste0(names(X), ".", dataIndex)
 M <- X
 
@@ -49,33 +43,32 @@ while (argIndex + 1 < length(args)) {
 	X <- read.csv(args[argIndex])
 
 	dataIndex <- dataIndex + 1
-	step <- 100 / length(X[[column]])
-	xvalues[[dataIndex]] <- seq(step, 100, step)
-	timeOrder <- order(X[[column]])
-	yvalues[[dataIndex]] <- X[[column]][timeOrder] / 1000
-	colorIndex <- colorIndex + 1
-	colors[[dataIndex]] <- colorIndex
-	legendStyle <- c(legendStyle, colorIndex)
-
 	names(X) <- paste0(names(X), ".", dataIndex)
-	M <- merge(M, X, all=TRUE, by.x=paste0(mergeBy, ".1"), by.y=paste0(mergeBy, ".", dataIndex))
-
+	M <- merge(M, X, by.x=paste0(mergeBy, ".1"), by.y=paste0(mergeBy, ".", dataIndex))
 }
-
 
 min.na.rm <- function(...) {
 	min(..., na.rm=TRUE)
 }
 M[[column]] <- apply(M[,paste0(column, ".", seq(dataIndex))], 1, min.na.rm)
-timeOrder <- order(M[[column]])
-step <- 100 / length(M[[column]])
-plot(seq(step, 100, step), M[[column]][timeOrder] / 1000, type="l", col=1, lty=1, log="y", main=title, xlab="\\% of queries", ylab="time in seconds", xlim=xlim, ylim=ylim)
+data <- M[[column]]
+timeOrder <- order(data)
+step <- 100 / length(data)
+xvalues <- seq(step, 100, step)
+plot(xvalues, data[timeOrder] / 1000, type="l", col=1, lty=1, log="y", main=title, xlab="\\% of queries", ylab="time in seconds", xlim=xlim, ylim=ylim)
 
+colorIndex <- 2
+legendStyle <- c()
+for (i in seq(dataIndex)) {
+	data <- M[[paste0(column, ".", i)]]
+	timeOrder <- order(data)
+	step <- 100 / length(data)
+	xvalues <- seq(step, 100, step)
+	lines(xvalues, data[timeOrder] / 1000, col=colorIndex, lty=colorIndex)
 
-for (i in 1:length(xvalues)) {
-	lines(xvalues[[i]], yvalues[[i]], col=colors[[i]], lty=colors[[i]])
+	legendStyle <- c(legendStyle, colorIndex)
+	colorIndex <- colorIndex + 1
 }
-
 
 legend <- c(legend, "minimal")
 legendStyle <- c(legendStyle, 1)
